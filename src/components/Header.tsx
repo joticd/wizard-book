d777ae66b36ff2dd99ab3b7e3914359b8cccc754
joticd <joticd@gmail.com>
2021-02-14 18:02:51 +0100

    fix:header css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,12 +8,12 @@ const Header:React.FC = () => {
                 <p>Add book - New book</p>
             </div>
 
-            <div className="progress">
-                <div className="progress-single active"><h3>1</h3><p>Genre</p></div>
+            <div className="d-flex flex-row progressbar">
+                <div className="progressbar-single active"><h3>1</h3><p>Genre</p></div>
                 <div className="line"></div>
-                <div className="progress-single"><h3>2</h3><p>Subgenre</p></div>
+                <div className="progressbar-single"><h3>2</h3><p>Subgenre</p></div>
                 <div className="line"></div>
-                <div className="progress-single"><h3>...</h3><p></p></div>
+                <div className="progressbar-single"><h3>...</h3><p></p></div>
             </div>
         </header>
     )
